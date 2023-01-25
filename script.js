d52021352717301c04de4e5881c7b8605fbc5e9b
Nabil <darknab@live.fr>
2023-01-25 16:38:50 +0100

    Gatherd all calculations inside a function calculator()

--- script.js
+++ script.js
@@ -56,6 +56,110 @@ function displayResult(result) {
 let display = 0, current = 0 , num1 = null, num2 = null, operand = ""; 
 let entry = {type : 0, value: 0}; result = 0
 
+function calculator(e) {
+    if (entry.type === "numeric") {
+        current += entry.value;
+        mainScreen.textContent = parseFloat(current);
+    } else if (entry.type === "operand") {
+        switch (e.target.id) {
+            
+            case "inverse":
+                num1 = parseFloat(current);
+                operand = "inverse"
+                result = operate(num1,operand);
+                upScreen.textContent = "1/" + num1 + " = ";
+                mainScreen.textContent = displayResult(result);
+                break;
+            case "root":
+                num1 = parseFloat(current);
+                operand = "root";
+                result = operate(num1,operand);
+                upScreen.textContent = "√" + num1 + " = ";
+                mainScreen.textContent = displayResult(result);
+                break;
+            case "squared":
+                num1 = parseFloat(current);
+                operand = "squared";
+                result = operate(num1,operand);
+                upScreen.textContent = num1 + "²" + " = ";
+                mainScreen.textContent = displayResult(result);
+                break;
+            case "percent":
+                if (operand === null || current === 0) {
+                    result = 0;
+                    upScreen.textContent = "0";
+                    mainScreen.textContent = "0";
+                    current = 0;
+                } else {
+                    if (operand === '*' || operand === "/") {
+                    current = parseFloat(current) / 100;
+                    console.log(operand);
+                }  else {
+                    if (operand === "+" || operand === "-") {
+                        current = (num1 * parseFloat(current))/100;
+                    }
+                }
+            }
+                    upScreen.textContent = num1 + " " + operand + " " + current;
+                
+                break;
+            default:
+            if (num1 === null) {
+                num1 = parseFloat(current);
+                operand = entry.value;
+                current = 0;
+                upScreen.textContent = num1 + " " + entry.value;
+            } else {
+                num2 = parseFloat(current);
+                result = operate(num1,operand,num2);
+                mainScreen.textContent = displayResult(result);
+                current = 0;
+                upScreen.textContent = result + " " + entry.value;
+                num1 = result;
+                operand = entry.value;
+            }
+    }
+    } else if (entry.type === "control") {
+        switch (e.target.id) {
+            case "negative":
+                const testNegative = current.substr(0,1);
+                if (testNegative === "-") {
+                    current = current.substr(1);
+                } else {current = "-" + current};
+                mainScreen.textContent = parseFloat(current);
+            break;
+            case "clear":
+                num1 = null;
+                num2 = null;
+                result = 0;
+                current = 0;
+                mainScreen.textContent = "0";
+                upScreen.textContent = "0";
+                break;
+            case "CE":
+                current = 0;
+                break;
+            case "del":
+                current = current.slice(0,current.length - 1);
+                mainScreen.textContent = parseFloat(current);
+                break;
+            case "equals":
+                if (num2 === null && current === 0) {
+                    result = operate(num1,operand,num1);
+                    mainScreen.textContent = displayResult(result);
+                    upScreen.textContent = `${num1} ${operand} ${num1} =`;
+                    current = 0; num1 = null; num2= null; operand = null;
+                } else {
+                    num2 = parseFloat(current);
+                    result = operate(num1,operand,num2);
+                    mainScreen.textContent = displayResult(result);
+                    upScreen.textContent = `${num1} ${operand} ${num2} =`;
+                    current = 0; num1 = null; num2= null; operand = null;
+                }
+                break;
+        }
+    }
+}
 
 const btn = document.querySelectorAll("button");
 const mainScreen = document.querySelector(".main");
@@ -66,110 +170,6 @@ btn.forEach((button) => {
     button.addEventListener("click", (e) => {
         entry.type = button.className;
         entry.value = button.textContent;
-        if (entry.type === "numeric") {
-            current += entry.value;
-            mainScreen.textContent = parseFloat(current);
-        } else if (entry.type === "operand") {
-            switch (e.target.id) {
-                
-                case "inverse":
-                    num1 = parseFloat(current);
-                    operand = "inverse"
-                    result = operate(num1,operand);
-                    upScreen.textContent = "1/" + num1 + " = ";
-                    mainScreen.textContent = displayResult(result);
-                    break;
-                case "root":
-                    num1 = parseFloat(current);
-                    operand = "root";
-                    result = operate(num1,operand);
-                    upScreen.textContent = "√" + num1 + " = ";
-                    mainScreen.textContent = displayResult(result);
-                    break;
-                case "squared":
-                    num1 = parseFloat(current);
-                    operand = "squared";
-                    result = operate(num1,operand);
-                    upScreen.textContent = num1 + "²" + " = ";
-                    mainScreen.textContent = displayResult(result);
-                    break;
-                case "percent":
-                    if (operand === null || current === 0) {
-                        result = 0;
-                        upScreen.textContent = "0";
-                        mainScreen.textContent = "0";
-                        current = 0;
-                    } else {
-                        if (operand === '*' || operand === "/") {
-                        current = parseFloat(current) / 100;
-                        console.log(operand);
-                    }  else {
-                        if (operand === "+" || operand === "-") {
-                            current = (num1 * parseFloat(current))/100;
-                        }
-                    }
-                }
-                        upScreen.textContent = num1 + " " + operand + " " + current;
-                    
-                    break;
-                default:
-                if (num1 === null) {
-                    num1 = parseFloat(current);
-                    operand = entry.value;
-                    current = 0;
-                    upScreen.textContent = num1 + " " + entry.value;
-                } else {
-                    num2 = parseFloat(current);
-                    result = operate(num1,operand,num2);
-                    mainScreen.textContent = displayResult(result);
-                    current = 0;
-                    upScreen.textContent = result + " " + entry.value;
-                    num1 = result;
-                    operand = entry.value;
-                }
-        }
-        } else if (entry.type === "control") {
-            switch (e.target.id) {
-                case "negative":
-                    const testNegative = current.substr(0,1);
-                    console.log(testNegative)
-                    if (testNegative === "-") {
-                        current = current.substr(1);
-                    } else {current = "-" + current};
-                    mainScreen.textContent = parseFloat(current);
-                break;
-                case "clear":
-                    num1 = null;
-                    num2 = null;
-                    result = 0;
-                    current = 0;
-                    mainScreen.textContent = "0";
-                    upScreen.textContent = "0";
-                    break;
-                case "CE":
-                    current = 0;
-                    break;
-                case "del":
-                    current = current.slice(0,current.length - 1);
-                    mainScreen.textContent = parseFloat(current);
-                    break;
-                case "equals":
-                    if (num2 === null && current === 0) {
-                        result = operate(num1,operand,num1);
-                        mainScreen.textContent = displayResult(result);
-                        upScreen.textContent = `${num1} ${operand} ${num1} =`;
-                        current = 0; num1 = null; num2= null; operand = null;
-                    } else {
-                        num2 = parseFloat(current);
-                        result = operate(num1,operand,num2);
-                        mainScreen.textContent = displayResult(result);
-                        upScreen.textContent = `${num1} ${operand} ${num2} =`;
-                        current = 0; num1 = null; num2= null; operand = null;
-                    }
-                    break;
-            }
-        }
+        calculator(e)
     })
 })
-
-
